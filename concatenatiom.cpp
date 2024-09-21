38fbcf6b15bcb241377b919f99a802228b00479c
shakibh10~ <billalsikdar03@gmail.com~>
2024-09-21 08:18:04 +0600

    first commit

---
+++ concatenatiom.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // string concatenation
+
+    string firstName="Shakib";
+    string lastName="Hasan";
+    string fullName=firstName + " " + lastName;
+
+    cout << fullName;
+
+    return 0;
+}
