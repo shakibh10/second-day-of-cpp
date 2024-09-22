d5fa17ce46f176c3faf52ae01772ac39fbb9fed2
shakibh10~ <billalsikdar03@gmail.com~>
2024-09-22 08:39:05 +0600

    addition Vs concatenation

---
+++ additionCon.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // addition
+
+    int x=4;
+    int y=5;
+
+    int addition=x+y;
+
+
+    string value1="20";
+    string value2="24";
+
+    string con=value1 + value2;
+
+    cout << addition << "\n";
+    cout << con;
+
+
+    return 0;
+}
